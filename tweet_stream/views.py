a5c6e1c7b2009c0234c4a829cea743d6036d6753
mcriqui <maggie.criqui@gmail.com>
2015-05-01 13:31:53 -0400

    created view to see template

--- tweet_stream/views.py
+++ tweet_stream/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def twitter_template(request):
+    return render(request, 'landing_page.html')
+
+def test(request):
+    return render(request, 'test.html')
