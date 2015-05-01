316a71dde8afebe3e1c28aac8cf2a5e299913189
mcriqui <maggie.criqui@gmail.com>
2015-05-01 11:06:18 -0400

    created tweet_stream app

---
+++ tweet_stream/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from tweet_stream import views
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', include(admin.site.urls)),
+    url(r'^$', views.twitter_template, name='twitter_template')
+)
